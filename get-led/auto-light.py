313e6f372413505710694c694cf86d60928870a5
Yaroslav Sychev <oriksokol@gmail.com>
2026-02-11 13:40:48 +0300

    auto-light

---
+++ get-led/auto-light.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+led=17
+GPIO.setup(led,GPIO.OUT)
+botton = 6
+state=0
+GPIO.setup(botton,GPIO.IN)
+while True:
+    state= GPIO.input(botton)
+    GPIO.output(led,state)
