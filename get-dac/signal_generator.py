5fc5f84725ddc83c13b960ef88b974c1b9da4bf2
Yaroslav Sychev <oriksokol@gmail.com>
2026-03-04 13:50:34 +0300

    r2r_adc

--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -1,7 +1,6 @@
 import numpy as np, time as tm
-def git_sin_wave_amplitude(freq,t=0.0):
+def git_sin_wave_amplitude(freq,t):
     s=np.sin(2*np.pi*freq*t)
-    t+=1/freq
     n_ampl=(s+1)/2
     return n_ampl
 def wait_for_sampling_period(sampling_frequency):
